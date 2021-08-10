1ef08205d9af5c6cae7d887ec5c0b8dca8868119
Luiz Miguel <luizmiguelproenca@gmail.com>
2021-08-09 21:48:47 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import logo from './assets/shopper.png'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+function App(){
+
+  return(
+    <View>
+      <Image source={logo} style={styles.imagem}/>
+      <Text style={styles.titulo}><FontAwesome name="user-circle" size={50} /> Olá Mundo!</Text>
+      <FontAwesome.Button
+        name="shopping-cart"
+        style={styles.botao}
+      >Comprar</FontAwesome.Button>
     </View>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  imagem: {
+    width: "90%",
+    height: 300,
+    resizeMode: 'center'
   },
-});
+  botao: {
+    width: '50%',
+    height: 30
+  },
+  titulo: {
+    fontSize: 30,
+    color: '#1A237E'
+  }
+})
+
+export default App
